369d894ab34f70f4e6893129b558058c88e20889
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-07-10 22:33:42 +0900

    design: Add GlobalStyle

---
+++ flex-ui/src/components/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+
+   body {
+    font-family: 'Noto Sans KR', sans-serif;
+   }
+`;
+
+export default GlobalStyle;
